174e5cf9d2498dc22019745ea23becf68719c60d
PRCinguhou <pingruchou1125tw@gmail.com>
2019-07-21 15:26:28 +0800

    add

--- model.py
+++ model.py
@@ -11,7 +11,6 @@ class refine_net(nn.Module):
 			nn.Conv2d(1, 8, 5, 1, 2),
 			nn.BatchNorm2d(8),
 			nn.ReLU(True),
-			nn.MaxPool2d(2)
 			)
 
 		# 256 x 256
@@ -19,7 +18,6 @@ class refine_net(nn.Module):
 			nn.Conv2d(8, 16, 5, 1, 2),
 			nn.BatchNorm2d(16),
 			nn.ReLU(True),
-			nn.MaxPool2d(2)
 			)
 
 		# 128 x 128
@@ -27,7 +25,6 @@ class refine_net(nn.Module):
 			nn.Conv2d(16, 32, 5, 1, 2),
 			nn.BatchNorm2d(32),
 			nn.ReLU(True),
-			nn.MaxPool2d(2)
 			)
 
 		# 64 x 64
@@ -35,54 +32,38 @@ class refine_net(nn.Module):
 			nn.Conv2d(32, 64, 5, 1, 2),
 			nn.BatchNorm2d(64),
 			nn.ReLU(True),
-			nn.MaxPool2d(2)
 			)
 
-		self.rcu4 = nn.Sequential(
-			nn.ReLU(True),
+		self.encoder5 = nn.Sequential(
 			nn.Conv2d(64, 64, 5, 1, 2),
+			nn.BatchNorm2d(64),
+			nn.ReLU(True),
 			)
 
-		self.rcu3 = nn.Sequential(
-			nn.ReLU(True),
-			nn.Conv2d(32, 32, 5, 1, 2),
-			)
-		
-		self.rcu2 = nn.Sequential(
-			nn.ReLU(True),
-			nn.Conv2d(16, 16, 5, 1, 2),
-			)
-		
-		self.rcu1 = nn.Sequential(
-			nn.ReLU(True),
-			nn.Conv2d(8, 8, 5, 1, 2),
-			)
-		
-
-		self.muti_fusion3 = nn.Sequential(
-			nn.ConvTranspose2d(64, 32, 4, 2, 1),
+		self.encoder6 = nn.Sequential(
+			nn.Conv2d(64, 32, 5, 1, 2),
 			nn.BatchNorm2d(32),
-			nn.ReLU(True)
+			nn.ReLU(True),
 			)
 
-		self.muti_fusion2 = nn.Sequential(
-			nn.ConvTranspose2d(32, 16, 4, 2, 1),
+		self.encoder7 = nn.Sequential(
+			nn.Conv2d(32, 16, 5, 1, 2),
 			nn.BatchNorm2d(16),
-			nn.ReLU(True)
+			nn.ReLU(True),
 			)
 
-		self.muti_fusion1 = nn.Sequential(
-			nn.ConvTranspose2d(16, 8, 4, 2, 1),
+		self.encoder8 = nn.Sequential(
+			nn.Conv2d(16, 8, 5, 1, 2),
 			nn.BatchNorm2d(8),
-			nn.ReLU(True)
+			nn.ReLU(True),
 			)
 
-		
-		self.final = nn.Sequential(
-			nn.ConvTranspose2d(8, 1, 4, 2, 1),
+		self.encoder9 = nn.Sequential(
+			nn.Conv2d(8, 1, 5, 1, 2),
 			nn.BatchNorm2d(1),
-			nn.Sigmoid()
+			nn.ReLU(True),
 			)
+
 
 
 	def forward(self, img):
@@ -94,16 +75,10 @@ class refine_net(nn.Module):
 		e3 = self.encoder3(e2)
 		# 64 x 32 x 32
 		e4 = self.encoder4(e3)
-		
-		# 64 x 32 x 32
-		res = self.rcu4(e4) + e4.detach()
-		# 32 x 64 x 64
-		res = self.rcu3(e3) + e3.detach() + self.muti_fusion3(res).detach()
-		# 16 x 128 x 128
-		res = self.rcu2(e2) + e2.detach() + self.muti_fusion2(res).detach()
-		# 8 x 256 x 256
-		res = self.rcu1(e1) + e1.detach() + self.muti_fusion1(res).detach()
-		
-		res = self.final(res)
+		e5 = self.encoder5(e4)
+		e6 = self.encoder6(e5)
+		e7 = self.encoder7(e6)
+		e8 = self.encoder8(e7)
+		e9 = self.encoder9(e8)
 
-		return res
+		return e9
